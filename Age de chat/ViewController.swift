3debac7e35fd0fcae51c69b9acbb1471b55a9a83
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-09-22 17:43:57 +0200

    Commit Application terminée

--- Age de chat/ViewController.swift
+++ Age de chat/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import AVFoundation                 // audio and video Foundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func calculerAction(_ sender: Any) {     // --------- Code quand bouton appuyé --------- //
+        
+        view.endEditing(true)           // Ranger le clavier
+        
+        // Vérifier que l'utilisateur à rentrer du texte
+        if ageTextField.text != nil {
+            let texte = ageTextField.text!
+            if let nombreEntier = Int(texte) {
+                let ageDeChat = nombreEntier * 7
+                resultLabel.text = "Votre âge de chat est de : \(ageDeChat)"
+            }
+        }
+    }
+    
+    @IBAction func soundAction(_ sender: Any) {    //  ---------  Faire parler l'app  --------- //
+        if let texte = resultLabel.text {                       // Est-ce qu'on a récupéré un texte dans la résultat
+            let speech = AVSpeechSynthesizer()                  // Permet de synthétiser une voix
+            let utterance = AVSpeechUtterance(string: texte)    // utérance
+            utterance.rate = 0.5                                // vitesse à laquelle la voix va parler -->  0 = lent || 1 = rapide
+            utterance.voice = AVSpeechSynthesisVoice(language: "fr_FR") // Voix
+            speech.speak(utterance)
+        }
+    }
+    
+    
+    
 }
 
